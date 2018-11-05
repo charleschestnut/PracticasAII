caa5014f9bf69d70daf2d0281c9bb73916d0dba5
Eduardo del Palacio Lirola <eddydpyl@gmail.com>
2018-11-05 12:46:41 +0100

    Placeholder

---
+++ main.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+from tkinter import *
+from sqlite3 import *
+from urllib import request
+
+
+def main_window():
+    return #TODO: Show the main window
+
+
+def store_data():
+    return  # TODO: Store the data from retieve_data in the database
+
+
+def retrieve_data(d: str):
+    return  # TODO: retrieve the necessary data from the URL in parameter d as an array
+
+
+def retrieve_page(d: str):
+    return  # TODO: auxiliary method for retrieving the website as a websoup object
+
+
+def list_data():
+    return  # TODO: Retrieve the data from the database and show them in the interface
+
+def search_data(search_type: str):
+    return  # TODO: Search the database applying some filter
+
+def order_data(order_type:str):
+    return  # TODO: Sort the data from the database using some criteria
+
+
+if __name__ == "__main__": main_window()
