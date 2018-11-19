21d024207e3ac86dff2f89aff75b55019753905d
charleschestnut <carcasdel1@alumn.us.es>
2018-11-19 11:03:04 +0100

    Created Whoosh practica file

--- main.py
+++ main.py
@@ -1,166 +1,209 @@
-# -*- coding: utf-8 -*-
+'''
+Created on 7 nov. 2018
 
-from bs4 import BeautifulSoup
+@author: cami_
+'''
 from tkinter import *
-from sqlite3 import *
 from tkinter import messagebox
-from urllib import request
+import os
+from whoosh.index import create_in, open_dir
+from whoosh.fields import Schema, TEXT, KEYWORD, DATETIME, NUMERIC
+from whoosh.qparser import QueryParser
+import urllib.request
+from bs4 import BeautifulSoup
+import datetime
+
+dirindextemas = "IndexTemas"
+dirindexrespuestas = "IndexRespuestas"
 
 
-def main_window():
-    top = Tk()
-    store_view = Button(top, text="Almacenar Productos", command=store_data)
-    store_view.pack(side=LEFT)
-    list_view = Button(top, text="Ordenar por Precio Unitario", command=lambda: order_data())
-    list_view.pack(side=LEFT)
-    list_view = Button(top, text="Mostrar Marca", command=lambda: list_data())
-    list_view.pack(side=LEFT)
-    list_view = Button(top, text="Buscar Rebajas", command=lambda: search_data())
-    list_view.pack(side=LEFT)
-    exit_view = Button(top, text="Salir", command=lambda: exit())
-    exit_view.pack(side=LEFT)
-    top.mainloop()
+# Crea un indice desde los documentos contenidos en dirdocs
+# El indice lo crea en un directorio (dirindex)
+def index():
+    if not os.path.exists(dirindextemas):
+        os.mkdir(dirindextemas)
+
+    ix = create_in(dirindextemas, schema=get_schema_tema())
+    writer = ix.writer()
+
+    if not os.path.exists(dirindexrespuestas):
+        os.mkdir(dirindexrespuestas)
+
+    ix2 = create_in(dirindexrespuestas, schema=get_schema_respuesta())
+    writer2 = ix2.writer()
+
+    # Extraemos los datos usando BeautifulSoup
+    url = urllib.request.urlopen("https://foros.derecho.com/foro/20-Derecho-Civil-General")
+    soup = BeautifulSoup(url, 'html.parser')
+    url.close()
+
+    items = soup.findAll(class_="threadinfo")
+
+    i = 0
+    x = 0
+    for index, item in enumerate(items):
+        if index != 0:
+            titulo = item.findAll(class_="title")[0].text
+            enlace = "https://foros.derecho.com/" + item.findAll(class_="title", href=True)[0]['href'].split("?")[0]
+            autor = item.findAll(class_="username understate")[0].text
+            fecha = item.find(class_="author").span.text.split(",")[1].strip()
+            fecha = datetime.datetime.strptime(fecha, "%d/%m/%Y %H:%M")
+            numRespuestas = soup.findAll(class_="threadstats td alt")[index - 1].li.a.text
+            numVisitas = \
+                soup.findAll(class_="threadstats td alt")[index - 1].li.find_next_sibling("li").text.split(":")[
+                    1].strip().replace(",", "")
+            writer.add_document(titulo=titulo, link=enlace, autor=autor, fecha=fecha, numRespuestas=numRespuestas,
+                                numVisitas=numVisitas)
+            i += 1
+
+            # Guardamos las respuestas
+            urlResCopia = enlace
+            urlRes = urllib.request.urlopen(enlace.encode('ascii', 'ignore').decode('ascii'))
+            soupRes = BeautifulSoup(urlRes, 'html.parser')
+            urlRes.close()
+
+            items2 = soupRes.findAll(class_="postcontainer")
+            for b, item2 in enumerate(items2):
+                itemX = item2.find(class_="postcontent")
+                user = item2.find(class_="username")
+                date = item2.find(class_="date")
+                response = itemX.text
+                user = user.text
+                date = datetime.datetime.strptime(date.text.replace(",", ""), "%d/%m/%Y %H:%M")
+                writer2.add_document(link=urlResCopia, fecha=date, texto=response, autor=user)
+                x += 1
+
+    messagebox.showinfo("Temas indexados", "Se han indexado " + str(i) + " temas")
+    messagebox.showinfo("Respuestas indexadass", "Se han indexado " + str(x) + " respuestas")
+    writer2.commit()
+    writer.commit()
 
 
-def store_data():
-    conn = connect('test.db')
-    conn.text_factory = str
-    conn.execute("DROP TABLE IF EXISTS PRODUCTOS")
-    conn.execute('''CREATE TABLE PRODUCTOS (
-            ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            DENOMINACION TEXT NOT NULL,
-            MARCA TEXT NOT NULL,
-            PRECIO_KILO DOUBLE NOT NULL,
-            PRECIO_ANTIGUO DOUBLE,
-            PRECIO_FINAL DOUBLE NOT NULL);''')
-    arbol = retrieve_page("https://www.ulabox.com/en/campaign/productos-sin-gluten#gref")
-    denominaciones, marcas, precio_vars, precios, precio_ants = retrieve_data(arbol)
-    contador = 0
-    for i in denominaciones:
-        cursor = conn.execute(
-            """INSERT INTO PRODUCTOS (DENOMINACION, MARCA, PRECIO_KILO, PRECIO_ANTIGUO, PRECIO_FINAL) VALUES (?,?,?,?,?)""",
-            (denominaciones[contador], marcas[contador], precio_vars[contador], precio_ants[contador], precios[contador]))
-        contador = contador + 1
-        cursor.close()
-    conn.commit()
-    cursor = conn.execute("SELECT COUNT(*) FROM PRODUCTOS")
-    values = cursor.fetchone()
-    popup_count(values[0])
-    conn.close()
+def get_schema_tema():
+    return Schema(titulo=TEXT(stored=True), link=TEXT(stored=True), autor=TEXT(stored=True),
+                  fecha=DATETIME(stored=True), numRespuestas=NUMERIC(stored=True), numVisitas=NUMERIC(stored=True))
 
 
-def popup_count(count: int):
-    win = Toplevel()
-    win.wm_title("Resultado")
-
-    l = Label(win, text="Se han añadido " + str(count) + " entradas.")
-    l.grid(row=0, column=0)
-
-    b = Button(win, text="Ok", command=win.destroy)
-    b.grid(row=1, column=0)
+def get_schema_respuesta():
+    return Schema(link=TEXT(stored=True), fecha=DATETIME(stored=True), texto=TEXT(stored=True), autor=TEXT(stored=True))
 
 
-def retrieve_data(arbol):
-    def modificar_precio(precio: str):
-        real_precio = precio[4:8]
-        real_precio = real_precio.strip()
-        real_precio = real_precio.replace(",", ".")
-        real_precio = float(real_precio)
-        return real_precio
-
-    # Filtramos para tener directamente la lista de PRODUCTOS
-    productos = arbol.find("div", id="product-list")
-    denominaciones = []
-    marcas = []
-    precio_vars = []
-    precios = []
-    precio_ants = []
-
-    for producto in productos.find_all("div", class_="grid__item m-one-whole t-one-third d-one-third dw-one-quarter | js-product-grid-grid"):
-        denominacion = producto.find('h3').find('a')
-        denominaciones.append(denominacion.text.strip())
-        print(denominacion.text.strip())
-
-        marca = producto.find('h4').find('a')
-        marcas.append(marca.text.strip())
-        print(marca.text.strip())
-
-        precio_var = producto.find('small', class_="product-item__ppu nano")
-        precio_var = modificar_precio(precio_var.text)
-        precio_vars.append(precio_var)
-        print(precio_var)
-
-        precio_int = producto.find('span', class_='delta')
-        precio_dec = producto.find('span', class_="milli")
-        precio = int(precio_int.text) + 0.01 * float(precio_dec.text[1:-1].strip())
-        precios.append(precio)
-
-        if len(producto.find("span", class_="product-grid-footer__price").contents) == 2:
-            precio_ant = producto.find("span", class_="product-grid-footer__price").find_all("del")[0].string
-        else:
-            precio_ant = None
-        precio_ants.append(precio_ant)
-
-    return denominaciones, marcas, precio_vars, precios, precio_ants
-
-
-def retrieve_page(d: str):
-    file = request.urlopen(d)
-    doc = BeautifulSoup(file, "html.parser")
-    return doc
-
-
-def list_data():
-    def list():
-        cursor = conn.execute('''SELECT * FROM PRODUCTOS WHERE MARCA LIKE ?''', (w.get(),))
-        print_product(cursor)
-
-    conn = connect('test.db')
-    brands = conn.execute('''SELECT MARCA FROM PRODUCTOS''')
+def searchByTitle():
+    def mostrar_lista(event):
+        lb.delete(0, END)  # borra toda la lista
+        ix = open_dir(dirindextemas)
+        with ix.searcher() as searcher:
+            query = QueryParser("titulo", ix.schema).parse("%" + str(en.get()) + "%")
+            results = searcher.search(query)
+            for r in results:
+                lb.insert(END, r['titulo'])
+                lb.insert(END, r['autor'])
+                lb.insert(END, r['fecha'])
+                lb.insert(END, '')
 
     v = Toplevel()
-    label = Label(v, text="Buscar Marca: ")
-    values = []
-    for i in brands:
-        values.append(i)
-    w = Spinbox(v, values=(values))
-
-    button = Button(v, text="¡Buscar!", command=list)
-    label.grid(row=0, column=0)
-    w.grid(row=0, column=1)
-    button.grid(row=1, column=1)
-
-
-def search_data():
-    conn = connect('test.db')
-    cursor = conn.execute('''SELECT * FROM PRODUCTOS WHERE PRECIO_ANTIGUO !=  NULL''')
-    print_product(cursor)
-    conn.close()
-
-
-def order_data():
-    conn = connect('test.db')
-    conn.text_factory = str
-    cursor = conn.execute("""SELECT * FROM PRODUCTOS ORDER BY PRECIO_KILO ASC""")
-    print_product(cursor)
-    conn.close()
-
-
-def print_product(cursor):
-    v = Toplevel()
+    v.title("Búsqueda por títulos")
+    f = Frame(v)
+    f.pack(side=TOP)
+    l = Label(f, text="Introduzca una palabra:")
+    l.pack(side=LEFT)
+    en = Entry(f)
+    en.bind("<Return>", mostrar_lista)
+    en.pack(side=LEFT)
     sc = Scrollbar(v)
     sc.pack(side=RIGHT, fill=Y)
-    lb = Listbox(v, width=150, yscrollcommand=sc.set)
-    if cursor != None:
-        for row in cursor:
-            lb.insert(END, "Nombre: " + str(row[1]))
-            lb.insert(END, "Precio antiguo: " + str(row[4]))
-            lb.insert(END, "Precio final: " + str(row[5]))
-            lb.insert(END, '')
-    else:
-        messagebox.showinfo("Error", "No se obtuvieron resultados...")
-    lb.pack(side=LEFT, fill=BOTH)
+    lb = Listbox(v, yscrollcommand=sc.set)
+    lb.pack(side=BOTTOM, fill=BOTH)
     sc.config(command=lb.yview)
 
 
-if __name__ == "__main__": main_window()
+def searchByAuthor():
+    def mostrar_lista(event):
+        lb.delete(0, END)  # borra toda la lista
+        ix = open_dir(dirindextemas)
+        with ix.searcher() as searcher:
+            query = QueryParser("autor", ix.schema).parse(str(en.get()))
+            results = searcher.search(query)
+            for r in results:
+                lb.insert(END, r['titulo'])
+                lb.insert(END, r['autor'])
+                lb.insert(END, r['fecha'])
+                lb.insert(END, '')
+
+    v = Toplevel()
+    v.title("Búsqueda por autor")
+    f = Frame(v)
+    f.pack(side=TOP)
+    l = Label(f, text="Introduzca un autor:")
+    l.pack(side=LEFT)
+    en = Entry(f)
+    en.bind("<Return>", mostrar_lista)
+    en.pack(side=LEFT)
+    sc = Scrollbar(v)
+    sc.pack(side=RIGHT, fill=Y)
+    lb = Listbox(v, yscrollcommand=sc.set)
+    lb.pack(side=BOTTOM, fill=BOTH)
+    sc.config(command=lb.yview)
+
+
+def searchByText():
+    def mostrar_lista(event):
+        lb.delete(0, END)  # borra toda la lista
+        ix = open_dir(dirindexrespuestas)
+        ix2 = open_dir(dirindextemas)
+        with ix.searcher() as searcher:
+            query = QueryParser("texto", ix.schema).parse("%" + en.get() + "%")
+            results = searcher.search(query)
+            for r in results:
+                with ix2.searcher() as searcher2:
+                    query = QueryParser("link", ix.schema).parse(r['link'])
+                    results2 = searcher2.search(query)
+                    for r2 in results2:
+                        lb.insert(END, r2['titulo'])
+                        lb.insert(END, r2['autor'])
+                        lb.insert(END, r2['fecha'])
+                        lb.insert(END, '')
+
+    v = Toplevel()
+    v.title("Búsqueda por respuestas")
+    f = Frame(v)
+    f.pack(side=TOP)
+    l = Label(f, text="Introduzca una palabra:")
+    l.pack(side=LEFT)
+    en = Entry(f)
+    en.bind("<Return>", mostrar_lista)
+    en.pack(side=LEFT)
+    sc = Scrollbar(v)
+    sc.pack(side=RIGHT, fill=Y)
+    lb = Listbox(v, yscrollcommand=sc.set)
+    lb.pack(side=BOTTOM, fill=BOTH)
+    sc.config(command=lb.yview)
+
+
+if __name__ == '__main__':
+    root = Tk()
+
+    root.geometry("270x100+50+50")
+
+    menubar = Menu(root)
+
+    helpmenu = Menu(menubar)
+    menuinicio = Menu(menubar)
+    nested_menu1 = Menu(helpmenu)
+    nested_menu1.add_command(label='Título', command=searchByTitle)
+    nested_menu1.add_command(label='Autor', command=searchByAuthor)
+    menuinicio.add_command(label='Indexar', command=index)
+    menuinicio.add_command(label='Salir', command=root.quit)
+    nested_menu2 = Menu(helpmenu)
+    nested_menu2.add_command(label='Texto', command=searchByText)
+
+    menu2_nested = Menu(nested_menu2)
+
+    helpmenu.add_cascade(label='Temas', menu=nested_menu1)
+    helpmenu.add_cascade(label='Respuestas', menu=nested_menu2)
+
+    menubar.add_cascade(label="Inicio", menu=menuinicio)
+    menubar.add_cascade(label="Buscar", menu=helpmenu)
+
+    root.config(menu=menubar)
+    root.mainloop()
